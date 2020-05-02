84cb3296d6a646ec36aa250e79043a3175cd40bd
daniel aranias <danielaranias@daniels-Air.lan>
2020-05-02 15:30:07 +0300

    trying to structure

---
+++ SpotifyAPIs/SpotifyClientAPI.py
@@ -0,0 +1,10 @@
+
+
+class SpotifyClientAPI():
+    """
+    """
+    def __init__(self):
+        pass
+    
+    def foo(self):
+        return True
---
+++ Tests/test_SpotifyClientAPI.py
@@ -0,0 +1,11 @@
+
+import pytest
+
+#import bla
+import foo 
+
+
+def test_foo():
+    #a = bla.bla() #this OK
+    b = foo.fff()
+    assert 1 == True
