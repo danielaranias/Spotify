1cb86dcd4a20d4896897528b6505a4e6d7b48206
daniel aranias <danielaranias@daniels-Air.lan>
2020-05-24 12:33:57 +0300

    refactoring - sending up to 100 IDs in one API

--- SpotiApps/SortifyApp/Sortify.py
+++ SpotiApps/SortifyApp/Sortify.py
@@ -1,6 +1,7 @@
 
 import os
 import sys
+import pandas as pd
 
 sys.path.append('../')
 
@@ -61,6 +62,48 @@ class Sortify():
             self.isSavedTracksAvailable =  False
 
         return self.saved_tracks_list
+    def tracksToDataFrame(self,tracksList)->"dataframe":
+        """
+            "duration_ms" : 255349,
+            "key" : 5,
+            "mode" : 0,
+            "time_signature" : 4,
+            "acousticness" : 0.514,
+            "danceability" : 0.735,
+            "energy" : 0.578,
+            "instrumentalness" : 0.0902,
+            "liveness" : 0.159,
+            "loudness" : -11.840,
+            "speechiness" : 0.0461,
+            "valence" : 0.624,
+            "tempo" : 98.002,
+            "id" : "06AKEBrKUckW0KREUWRnvT",
+            "uri" : "spotify:track:06AKEBrKUckW0KREUWRnvT",
+            "track_href" : 
+        """
+        
+        track_features_list = []
+        tracks_ids = []
+        tracks_names = []
+        
+        #collecting all IDs for getting later the features
+        for item in tracksList:
+            tracks_ids.append(item['track']['id'])
+            tracks_names.append(item['track']['name'])
+
+        #calling to spotify API to get tracks features (MAX 100) - much faster than one by one
+        track_index = 0
+        tracks_len = len(tracks_ids)
+    
+        for track_index in range(0, tracks_len, 100):
+            track_features_list += self.sp_data.audio_features(tracks_ids[track_index:(track_index+100)])
+            
+
+        df = pd.DataFrame(track_features_list)
+
+        return df
+
+        
 
     def showTracks(self,results):
         for item in results:
